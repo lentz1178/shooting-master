f18def15558129358ded96a32b69dc8e05bad977
Levi Entz <levientz388@gmail.com>
2025-11-03 17:55:12 +0000

    removed space from file name so that files can be found

--- main.py/script.py
+++ main.py/script.py
@@ -24,10 +24,10 @@ for i in range(1, 4):
     gun.append(pygame.transform.scale(pygame.image.load(f"assets/gun/{i}.png"), (100, 100)))
     if i < 3:
         for j in range(1, 4):
-            target_images[i - 1].append(pygame.transform.scale(pygame.image.load(f"assets/targets/{i}/ {j}.png"), (120 - (j * 18), 80 - (j*12))))
+            target_images[i - 1].append(pygame.transform.scale(pygame.image.load(f"assets/targets/{i}/{j}.png"), (120 - (j * 18), 80 - (j*12))))
     else:
         for j in range(1, 5):
-            target_images[i - 1].append(pygame.transform.scale(pygame.image.load(f"assets/targets/{i}/ {j}.png"), (120 - (j * 18), 80 - (j*12))))
+            target_images[i - 1].append(pygame.transform.scale(pygame.image.load(f"assets/targets/{i}/{j}.png"), (120 - (j * 18), 80 - (j*12))))
 
 
 def draw_gun():
